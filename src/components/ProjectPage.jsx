515d34e283c4aa33e324f5e9848ad902e868857d
gbvivian <gbvivian@hotmail.com>
2019-07-07 16:53:19 -0700

    Update ProjectPage to include text

--- src/components/ProjectPage.jsx
+++ src/components/ProjectPage.jsx
@@ -13,8 +13,14 @@ export default function ProjectPage() {
 				<Grid item xs={12}>
 					<img src={Project1Img} alt="Project1Img" width="1440px" height="1080px" fluid="true" />
 				</Grid>
-				<Grid item xs={4}>
+				<Grid item xs={6}>
 					<h1>Title</h1>
+				</Grid>
+			</Grid>
+
+			<Grid container spacing={24} justify="center" alignItems="center">
+				<Grid item xs={6}>
+					<p>Text</p>
 				</Grid>
 			</Grid>
 		</React.Fragment>
