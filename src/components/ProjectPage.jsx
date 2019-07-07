9cdededafe95ec4a6874efe8a42213103386d60c
gbvivian <gbvivian@hotmail.com>
2019-07-07 16:47:36 -0700

    Add image to ProjectPage component

--- src/components/ProjectPage.jsx
+++ src/components/ProjectPage.jsx
@@ -1,6 +1,7 @@
 import Grid from '@material-ui/core/Grid';
 import NavBar from './Navbar';
 import React from 'react';
+import Project1Img from '../assets/Project1_Img.jpg';
 
 export default function ProjectPage() {
 	console.log('ProjectPage is called');
@@ -8,8 +9,12 @@ export default function ProjectPage() {
 		<React.Fragment>
 			<Grid container spacing={24} justify="center" alignItems="center">
 				<NavBar />
-				<Grid item xs={6}>
-					<h1>boof</h1>
+				<div />
+				<Grid item xs={12}>
+					<img src={Project1Img} alt="Project1Img" width="1440px" height="1080px" fluid="true" />
+				</Grid>
+				<Grid item xs={4}>
+					<h1>Title</h1>
 				</Grid>
 			</Grid>
 		</React.Fragment>
