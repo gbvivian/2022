150585dac231597000689ba9b898a782206ae515
gbvivian <gbvivian@hotmail.com>
2019-07-07 16:21:14 -0700

    Add Navbar to Boof

--- src/components/Boof.jsx
+++ src/components/Boof.jsx
@@ -1,9 +1,11 @@
+import NavBar from './Navbar';
 import React from 'react';
 
 export default function Boof() {
 	console.log('boof is called');
 	return (
 		<React.Fragment>
+			<NavBar />
 			<h1>boof</h1>
 		</React.Fragment>
 	);
