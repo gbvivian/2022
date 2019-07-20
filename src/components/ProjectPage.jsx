ed8d7bf9401f291b0cb5d486834307706f58d74d
gbvivian <gbvivian@hotmail.com>
2019-07-20 16:31:34 -0700

    Create margins for tile and body text: closes #3, closes #1

--- src/components/ProjectPage.jsx
+++ src/components/ProjectPage.jsx
@@ -7,8 +7,18 @@ export default function ProjectPage() {
 	console.log('ProjectPage is called');
 	return (
 		<React.Fragment>
-			<div className="w-screen md:mx-16 md:mt-16">
+			<div className="md:mx-16 md:mt-16">
 				<img src={Project1Img} alt="Project" />
+
+				<div className="flex flex-col md:mx-32">
+					<p className="text-6xl">I'm a title</p>
+					<div className="text-2xl">
+						Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic
+						or web designs. The passage is attributed to an unknown typesetter in the 15th century who is
+						thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type
+						specimen book. It usually begins with:
+					</div>
+				</div>
 			</div>
 		</React.Fragment>
 	);
