d127f9d7be9e25e206091a067e7547f406bb35ec
gbvivian <gbvivian@hotmail.com>
2019-07-20 14:14:30 -0700

    Remove white background from project cards

--- src/App.js
+++ src/App.js
@@ -95,18 +95,16 @@ function ProjectCard() {
 	return (
 		<React.Fragment>
 			<div className="w-full sm:w-5/12 m-10 sm:m-6 overflow-hidden">
-				<div className="px-6 py-4 bg-white">
-					<Link to="/proj">
-						Project
-						<div className="image">
-							<img className="w-full" to="/proj" src={Project1Img} alt="Project Image" />
-						</div>
-					</Link>
-					<p className="text-gray-700 text-base">
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et
-						perferendis eaque, exercitationem praesentium nihil.
-					</p>
-				</div>
+				<Link to="/proj">
+					<div className="image">
+						<img className="w-full" to="/proj" src={Project1Img} alt="Project Image" />
+					</div>
+					Project
+				</Link>
+				<p className="text-gray-700 text-base">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et
+					perferendis eaque, exercitationem praesentium nihil.
+				</p>
 			</div>
 		</React.Fragment>
 	);
