dad670dc6623f028b0ebaf8030b6836819acaa49
gbvivian <gbvivian@hotmail.com>
2019-07-20 17:25:52 -0700

    Update class to className

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,10 +5,10 @@ import { Link } from 'react-router-dom';
 export default function Navbar() {
 	return (
 		<React.Fragment>
-			<nav class="flex items-center justify-between flex-wrap p-6 text-gray-900">
-				<div class="flex items-center flex-shrink-0 mr-6">
+			<nav classNameName="flex items-center justify-between flex-wrap p-6 text-gray-900">
+				<div className="flex items-center flex-shrink-0 mr-6">
 					<svg
-						class="fill-current h-8 w-8 mr-2"
+						className="fill-current h-8 w-8 mr-2"
 						width="54"
 						height="54"
 						viewBox="0 0 54 54"
@@ -16,22 +16,22 @@ export default function Navbar() {
 					>
 						<path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
 					</svg>
-					<span class="font-semibold text-xl tracking-tight">Vivian Lam</span>
+					<span className="font-semibold text-xl tracking-tight">Vivian Lam</span>
 				</div>
-				<div class="block lg:hidden">
-					<button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-						<svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+				<div className="block lg:hidden">
+					<button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+						<svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
 							<title>Menu</title>
 							<path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
 						</svg>
 					</button>
 				</div>
 
-				<div class="w-full block flex-grow lg:flex lg:flex-row-reverse lg:items-center lg:w-auto">
-					<a href="/Resume-Vivian-Lam.pdf" class="block mt-4 lg:inline-block lg:mt-0 hover:text-gray-500">
+				<div className="w-full block flex-grow lg:flex lg:flex-row-reverse lg:items-center lg:w-auto">
+					<a href="/Resume-Vivian-Lam.pdf" className="block mt-4 lg:inline-block lg:mt-0 hover:text-gray-500">
 						Resume
 					</a>
-					<Link to="/about" class="block mt-4 mr-4 lg:inline-block lg:mt-0 hover:text-gray-500">
+					<Link to="/about" className="block mt-4 mr-4 lg:inline-block lg:mt-0 hover:text-gray-500">
 						About me
 					</Link>
 				</div>
