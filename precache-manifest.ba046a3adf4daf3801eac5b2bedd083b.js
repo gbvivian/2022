7b4989b700af2e6077f14a1d1fcd67c02a245902
gbvivian <gbvivian@hotmail.com>
2019-07-20 18:02:52 -0700

    Updates

---
+++ precache-manifest.ba046a3adf4daf3801eac5b2bedd083b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31b410ce4fc5b07bd78ac4719c7be369",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a31e9a1802483c4ed41",
+    "url": "/static/css/main.385dbd42.chunk.css"
+  },
+  {
+    "revision": "8dcf0a1b79547d263f3e",
+    "url": "/static/js/2.0a6169b6.chunk.js"
+  },
+  {
+    "revision": "9a31e9a1802483c4ed41",
+    "url": "/static/js/main.54e5a495.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b2f1f0318a0fa53e7b5868fbe766d1af",
+    "url": "/static/media/Project1_Img.b2f1f031.jpg"
+  }
+]);
