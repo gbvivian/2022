156ed10cd718defbf4f52846b7461b7021948249
gbvivian <gbvivian@hotmail.com>
2019-07-20 17:37:44 -0700

    Route to homepage onclick logo on navbar, closes #9

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,18 +5,11 @@ import { Link } from 'react-router-dom';
 export default function Navbar() {
 	return (
 		<React.Fragment>
-			<nav classNameName="flex items-center justify-between flex-wrap p-6 text-gray-900">
+			<nav className="flex items-center justify-between flex-wrap p-6 text-gray-900">
 				<div className="flex items-center flex-shrink-0 mr-6">
-					<svg
-						className="fill-current h-8 w-8 mr-2"
-						width="54"
-						height="54"
-						viewBox="0 0 54 54"
-						xmlns="http://www.w3.org/2000/svg"
-					>
-						<path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
-					</svg>
-					<span className="font-semibold text-xl tracking-tight">Vivian Lam</span>
+					<Link to="/">
+						<span className="font-semibold text-2xl tracking-tight">Vivian Lam</span>
+					</Link>
 				</div>
 				<div className="block lg:hidden">
 					<button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
