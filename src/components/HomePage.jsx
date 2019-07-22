0426f4b491f3e9de53daf22b972470aeb4c764e0
gbvivian <gbvivian@hotmail.com>
2019-07-21 19:10:54 -0700

    Create props for projectcard images

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ProjectCard from '../components/ProjectCard';
+import Project1Img from '../assets/Project1_Img.jpg';
 
 const HomePage = () => {
 	return (
@@ -8,8 +9,8 @@ const HomePage = () => {
 				<div className="text-3xl md:ml-8">Hi! My name is Vivian.</div>
 			</div>
 			<div className="flex flex-wrap justify-center">
-				<ProjectCard projectName="Project 1" />
-				<ProjectCard projectName="Project 2" />
+				<ProjectCard projectName="Project 1" projectImg={Project1Img} />
+				<ProjectCard projectName="Project 2" projectImg={Project1Img} />
 			</div>
 		</React.Fragment>
 	);
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Project1Img from '../assets/Project1_Img.jpg';
 
 const ProjectCard = (props) => {
 	return (
@@ -8,7 +7,7 @@ const ProjectCard = (props) => {
 			<div className="w-full sm:w-5/12 m-10 sm:m-6 md:m-15 lg:m-12 overflow-hidden">
 				<Link to="/proj">
 					<div className="image">
-						<img className="w-full" to="/proj" src={Project1Img} alt="Project" />
+						<img className="w-full" to="/proj" src={props.projectImg} alt="Project" />
 					</div>
 					<div className="hover:underline text-3xl mt-6">{props.projectName}</div>
 				</Link>
