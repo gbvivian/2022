46b131f7704616f9c4afb3b8fc75c91073a5a419
gbvivian <gbvivian@hotmail.com>
2019-07-21 18:33:40 -0700

    Update defaults

---
+++ build/precache-manifest.35b884e2bbf76f1ea1ca1b9ba1b49868.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b383e78c3c7637d4f889e97842b0b51f",
+    "url": "/portfolio-2019/index.html"
+  },
+  {
+    "revision": "1f00ff4a8170f6ce4041",
+    "url": "/portfolio-2019/static/css/main.385dbd42.chunk.css"
+  },
+  {
+    "revision": "4dad0fde7aba94345c06",
+    "url": "/portfolio-2019/static/js/2.a8c6b70b.chunk.js"
+  },
+  {
+    "revision": "1f00ff4a8170f6ce4041",
+    "url": "/portfolio-2019/static/js/main.fab68ef1.chunk.js"
+  },
+  {
+    "revision": "082e7cb3b386e751ce75",
+    "url": "/portfolio-2019/static/js/runtime~main.d36a5cc7.js"
+  },
+  {
+    "revision": "b2f1f0318a0fa53e7b5868fbe766d1af",
+    "url": "/portfolio-2019/static/media/Project1_Img.b2f1f031.jpg"
+  }
+]);
