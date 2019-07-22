e9a144255f304bf93806554158c042e4df9bdbef
gbvivian <gbvivian@hotmail.com>
2019-07-21 18:31:33 -0700

    Tweak project card margins

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -5,7 +5,7 @@ import Project1Img from '../assets/Project1_Img.jpg';
 const ProjectCard = () => {
 	return (
 		<React.Fragment>
-			<div className="w-full sm:w-5/12 m-10 sm:m-6 overflow-hidden">
+			<div className="w-full sm:w-5/12 m-10 sm:m-6 md:m-15 lg:m-12 overflow-hidden">
 				<Link to="/proj">
 					<div className="image">
 						<img className="w-full" to="/proj" src={Project1Img} alt="Project" />
