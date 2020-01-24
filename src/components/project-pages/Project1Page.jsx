736842c6ba4d6a49628a284f7685b7cc4ed48a73
Vivian Lam <vivian.lam@shopify.com>
2020-01-23 22:40:43 -0500

    Add font styling + fix margins in project 1 page

--- src/components/project-pages/Project1Page.jsx
+++ src/components/project-pages/Project1Page.jsx
@@ -9,10 +9,10 @@ export default function Project1Page() {
       <div className="md:mx-16 md:mt-16">
         <img src={Project1Img} alt="Project" />
 
-        <div className="flex flex-col md:mx-32">
-          <p className="text-6xl">Shopify Internship 2020</p>
-          <div className="text-4xl">Challenge</div>
-          <div className="text-2xl">
+        <div className="flex flex-col md:mx-64">
+          <p className="text-6xl mb-12">Shopify Internship 2020</p>
+          <div className="text-4xl font-semibold">Challenge</div>
+          <div className="text-2xl mb-8">
             When a user enters a discount code that gives free shipping, the
             information is reflected in the&nbsp;
             <span className="text-2xl font-semibold">
@@ -22,24 +22,24 @@ export default function Project1Page() {
             <span className="text-2xl font-semibold">Shipping details.</span>
           </div>
 
-          <div className="text-2xl">
+          <div className="text-2xl mb-8">
             The lack of consistent information may leave the user confused as to
             whether the shipping is free or not, which in turn can affect
             whether they decide to complete their purchase.
           </div>
 
-          <div className="text-2xl">
+          <div className="text-2xl mb-8">
             Additionally, it is not obvious that the discount code applies to
             the shipping fees.
           </div>
 
-          <div className="text-2xl">
+          <div className="text-2xl mb-8">
             How might we associate the discount code with the shipping fee, and
             how might we convey consistent information about shipping fees
             across the page?
           </div>
 
-          <div className="text-4xl">Solution</div>
+          <div className="text-4xl font-semibold">Solution</div>
           <img src={discount_refactor_demo} alt="Discount refactor demo" />
         </div>
       </div>
