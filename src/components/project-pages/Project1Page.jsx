af714132203d0320629ee629a71788cabe084c36
Vivian Lam <vivian.lam@shopify.com>
2020-01-23 22:21:37 -0500

    Populate project 1 page

--- src/components/project-pages/Project1Page.jsx
+++ src/components/project-pages/Project1Page.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Project1Img from '../../assets/Project1_Img.jpg';
+import challenge from '../../assets/project1/challenge.png';
+import discount_refactor_demo from '../../assets/project1/discount_refactor_demo.gif';
 
 export default function Project1Page() {
   return (
@@ -8,14 +10,37 @@ export default function Project1Page() {
         <img src={Project1Img} alt="Project" />
 
         <div className="flex flex-col md:mx-32">
-          <p className="text-6xl">I'm a title</p>
+          <p className="text-6xl">Shopify Internship 2020</p>
+          <div className="text-4xl">Challenge</div>
           <div className="text-2xl">
-            Lorem ipsum, or lipsum as it is sometimes known, is dummy text used
-            in laying out print, graphic or web designs. The passage is
-            attributed to an unknown typesetter in the 15th century who is
-            thought to have scrambled parts of Cicero's De Finibus Bonorum et
-            Malorum for use in a type specimen book. It usually begins with:
+            When a user enters a discount code that gives free shipping, the
+            information is reflected in the&nbsp;
+            <span className="text-2xl font-semibold">
+              Order summary section{' '}
+            </span>
+            <span className="text-2xl">but not the&nbsp;</span>
+            <span className="text-2xl font-semibold">Shipping details.</span>
           </div>
+
+          <div className="text-2xl">
+            The lack of consistent information may leave the user confused as to
+            whether the shipping is free or not, which in turn can affect
+            whether they decide to complete their purchase.
+          </div>
+
+          <div className="text-2xl">
+            Additionally, it is not obvious that the discount code applies to
+            the shipping fees.
+          </div>
+
+          <div className="text-2xl">
+            How might we associate the discount code with the shipping fee, and
+            how might we convey consistent information about shipping fees
+            across the page?
+          </div>
+
+          <div className="text-4xl">Solution</div>
+          <img src={discount_refactor_demo} alt="Discount refactor demo" />
         </div>
       </div>
     </React.Fragment>
