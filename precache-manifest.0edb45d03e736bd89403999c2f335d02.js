270830a7006751a8d344162de058f052c7a65c3c
Vivian Lam <vivian.lam@shopify.com>
2020-01-23 19:09:32 -0500

    Updates

---
+++ precache-manifest.0edb45d03e736bd89403999c2f335d02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b72fb23d55026c1206aeb09446e4aeee",
+    "url": "/portfolio-2019/index.html"
+  },
+  {
+    "revision": "e09f37946b2ae6fb0c5b",
+    "url": "/portfolio-2019/static/css/main.385dbd42.chunk.css"
+  },
+  {
+    "revision": "81740aa98ac1865c41ca",
+    "url": "/portfolio-2019/static/js/2.344f3dc8.chunk.js"
+  },
+  {
+    "revision": "2e7d2c37866ca6c066224e205e1b0aa3",
+    "url": "/portfolio-2019/static/js/2.344f3dc8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e09f37946b2ae6fb0c5b",
+    "url": "/portfolio-2019/static/js/main.1b2aae8a.chunk.js"
+  },
+  {
+    "revision": "cfab0be9e2f50d9ba905",
+    "url": "/portfolio-2019/static/js/runtime-main.2bb9aa57.js"
+  },
+  {
+    "revision": "b2f1f0318a0fa53e7b5868fbe766d1af",
+    "url": "/portfolio-2019/static/media/Project1_Img.b2f1f031.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio-2019/precache-manifest.35b884e2bbf76f1ea1ca1b9ba1b49868.js"
+  "/portfolio-2019/precache-manifest.0edb45d03e736bd89403999c2f335d02.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio-2019/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
