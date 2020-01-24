d26fe42727cb24b07117ca6a38434eefca0480c3
Vivian Lam <vivian.lam@shopify.com>
2020-01-23 23:14:40 -0500

    Updates

---
+++ precache-manifest.aac89ea58722b3906e42af7397fbf095.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7435467428ebba20b4f59b1375d3b33a",
+    "url": "/portfolio-2019/index.html"
+  },
+  {
+    "revision": "982a5e1e17c3fd48fc31",
+    "url": "/portfolio-2019/static/css/main.385dbd42.chunk.css"
+  },
+  {
+    "revision": "7c49ca3b5005ac6c567e",
+    "url": "/portfolio-2019/static/js/2.156dc28e.chunk.js"
+  },
+  {
+    "revision": "2e7d2c37866ca6c066224e205e1b0aa3",
+    "url": "/portfolio-2019/static/js/2.156dc28e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "982a5e1e17c3fd48fc31",
+    "url": "/portfolio-2019/static/js/main.8b4b49ce.chunk.js"
+  },
+  {
+    "revision": "cfab0be9e2f50d9ba905",
+    "url": "/portfolio-2019/static/js/runtime-main.2bb9aa57.js"
+  },
+  {
+    "revision": "b2f1f0318a0fa53e7b5868fbe766d1af",
+    "url": "/portfolio-2019/static/media/Project1_Img.b2f1f031.jpg"
+  },
+  {
+    "revision": "1060fbc9cc351f3aad9f3cd6dd68070b",
+    "url": "/portfolio-2019/static/media/challenge.1060fbc9.png"
+  },
+  {
+    "revision": "268a1792abb1864e3d86d8ff6643c0d3",
+    "url": "/portfolio-2019/static/media/discount_refactor_demo.268a1792.gif"
+  },
+  {
+    "revision": "4d9152171e25833723563a03b657debb",
+    "url": "/portfolio-2019/static/media/home_page.4d915217.png"
+  },
+  {
+    "revision": "1b9df69593b62cc30e07a22416b32207",
+    "url": "/portfolio-2019/static/media/notif_lockscreen.1b9df695.png"
+  },
+  {
+    "revision": "82c3bcf3e2bc747c42fb7f55cb99ec39",
+    "url": "/portfolio-2019/static/media/product_feature.82c3bcf3.png"
+  }
+]);
