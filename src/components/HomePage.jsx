02bcb52f452acb87cff5179d550ee96701c28777
Vivian Lam <gbvivian@hotmail.com>
2024-01-17 16:47:00 -0500

    MVP update: update font to jakarta, fluid responsive grid showing 3 projects

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,6 +3,7 @@ import ProjectCard from '../components/ProjectCard';
 import atomicLoansThumbnail from '../assets/atomic-loans/2024_Atomic_Finance_Thumbnail.png';
 import chimneyThumbnail from '../assets/chimney/2024_Chimney_Thumbnail.png';
 import shopPayThumbnail from '../assets/shop-pay/2024_Shop_Pay_Thumbnail.png';
+import shopifyBulkEditorThumbnail from '../assets/shopify-bulk-editor/2024_BE_Shopify_thumbnail.png';
 import sonicThumbnail from '../assets/sonic/2024_Web_Performance_Thumbnail.png';
 
 const HomePage = () => {
@@ -12,11 +13,14 @@ const HomePage = () => {
         {/* <div className="text-3xl mt-14">
               Product designer & Developer
             </div> */}
+        {/* 30px */}
+        <div className="font-bold text-3xl mt-16">VIVIAN LAM</div>
 
-        <div className="text-4xl mt-16 mb-12">
-          <b>Hi, my name is Vivian! 🌼</b>
-        </div>
-        <div className="text-2xl mt-4" style={{ maxWidth: '745px' }}>
+        {/* 20px */}
+        <div
+          className="text-zinc-400 text-xl mt-4"
+          style={{ maxWidth: '745px' }}
+        >
           Currently a designer at <span className="font-semibold">Shopify</span>{' '}
           and <span className="font-semibold">Chimney.ai</span>. In the past,
           I've worked as a developer at{' '}
@@ -24,11 +28,19 @@ const HomePage = () => {
           <span className="font-semibold">Motion Metrics</span> and{' '}
           <span className="font-semibold">IA Financial Group</span>.
         </div>
-        <div className="text-4xl mt-16 mb-12">
-          <b>Work</b>
-        </div>
+
+        <div className="font-bold text-2xl mt-16">WORK</div>
         {/* <div className="grid my-20 grid-flow-col gap-y-24 gap-x-24 grid-cols-1 grid-rows-4 xl:grid-cols-2 xl:grid-rows-2"> */}
-        <div className="grid grid-cols-4 gap-4">
+
+        <div
+          className="grid
+                          mt-5
+                          xl:grid-cols-3 gap-x-8 gap-y-2
+                          md:grid-cols-2 gap-8"
+        >
+          <div>
+            <ProjectCard projectImg={shopifyBulkEditorThumbnail} />
+          </div>
           <div>
             <ProjectCard
               projectImg={chimneyThumbnail}
