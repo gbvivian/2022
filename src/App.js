72a8c793b2fede3e8491cd2530ce90dee35be55d
Vivian Lam <gbvivian@hotmail.com>
2024-01-19 12:49:48 -0500

    Update thumbnails, change hover color to black, fix hover border radius

--- src/App.js
+++ src/App.js
@@ -24,23 +24,23 @@ function App() {
   const styles = {
     margins: {
       marginLeft: '20',
-      marginRight: '20'
-    }
+      marginRight: '20',
+    },
   };
 
   return (
     <Router basename="/">
-     <div id="app">
-      <div className='mx-10 mt-20 md:mx-20 lg:mx-40'>
-        <div className='flex'>
-        <Navbar />
-        </div>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/proj1" component={Project1Page} />
-        <Route exact path="/proj2" component={Project2Page} />
+      <div id="app">
+        <div className="mx-10 mt-20 md:mx-20 lg:mx-64">
+          <div className="flex">
+            <Navbar />
+          </div>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/proj1" component={Project1Page} />
+          <Route exact path="/proj2" component={Project2Page} />
 
-        <Route exact path="/about" component={() => <div>About</div>} />
-      </div>
+          <Route exact path="/about" component={() => <div>About</div>} />
+        </div>
       </div>
     </Router>
   );
