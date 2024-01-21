b037d3352bdc57e4a3ca4bce57b56ff1b755131e
Vivian Lam <gbvivian@hotmail.com>
2024-01-21 17:17:17 -0500

    Updates

---
+++ precache-manifest.bb73444ebce86cf17d7e3b0890858fae.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b0b369b214574fc07c42569f5f1dfdf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d86ba96ab457bb85fc9a",
+    "url": "/static/css/main.229bba5c.chunk.css"
+  },
+  {
+    "revision": "75d0a300567f12c089b5",
+    "url": "/static/js/2.3c240cde.chunk.js"
+  },
+  {
+    "revision": "d86ba96ab457bb85fc9a",
+    "url": "/static/js/main.4f4738f6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7488d6702173bdd4c836bf8ecb83d73b",
+    "url": "/static/media/2024_Atomic_Finance_Thumbnail.7488d670.png"
+  },
+  {
+    "revision": "6b9db4e6ff3ec3ed79f2ac8ed9c890ea",
+    "url": "/static/media/2024_BE_Shopify_thumbnail.6b9db4e6.png"
+  },
+  {
+    "revision": "1059f8adf7b5231dfb01284d6ba44d41",
+    "url": "/static/media/2024_Chimney_Thumbnail.1059f8ad.png"
+  },
+  {
+    "revision": "d71cabf341dd0c67e6342787773b99dd",
+    "url": "/static/media/2024_Shop_Pay_Thumbnail.d71cabf3.png"
+  },
+  {
+    "revision": "1284cfe938ee33b88388aeb62128ce69",
+    "url": "/static/media/2024_Web_Performance_Thumbnail.1284cfe9.png"
+  },
+  {
+    "revision": "b2f1f0318a0fa53e7b5868fbe766d1af",
+    "url": "/static/media/Project1_Img.b2f1f031.jpg"
+  },
+  {
+    "revision": "0095403d9b48f4a4416e7eeea77da109",
+    "url": "/static/media/Resume_Vivian_Lam.0095403d.pdf"
+  },
+  {
+    "revision": "1060fbc9cc351f3aad9f3cd6dd68070b",
+    "url": "/static/media/challenge.1060fbc9.png"
+  },
+  {
+    "revision": "4835d628d354628914903e7bc0c4f056",
+    "url": "/static/media/cm.4835d628.png"
+  },
+  {
+    "revision": "268a1792abb1864e3d86d8ff6643c0d3",
+    "url": "/static/media/discount_refactor_demo.268a1792.gif"
+  },
+  {
+    "revision": "e2b2af3f8d9e677e77fb6d0e46a82989",
+    "url": "/static/media/dt.e2b2af3f.png"
+  },
+  {
+    "revision": "4d9152171e25833723563a03b657debb",
+    "url": "/static/media/home_page.4d915217.png"
+  },
+  {
+    "revision": "1b9df69593b62cc30e07a22416b32207",
+    "url": "/static/media/notif_lockscreen.1b9df695.png"
+  },
+  {
+    "revision": "82c3bcf3e2bc747c42fb7f55cb99ec39",
+    "url": "/static/media/product_feature.82c3bcf3.png"
+  },
+  {
+    "revision": "f94594c54a7fc59006e529c5edaf790e",
+    "url": "/static/media/rs.f94594c5.png"
+  }
+]);
